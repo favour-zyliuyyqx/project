0faa95d167220a7266af99012b6871d2f8bce898
favour-zyliuyyqx <442855532@qq.com>
2020-04-08 23:24:47 +0800

    project

---
+++ fact.py
@@ -0,0 +1,13 @@
+"myfact module"
+
+def factorial(num):
+    """
+    :arg num:
+    :return:
+    """
+    if num >= 0:
+        if num == 0:
+            return 1
+        return num * factorial(num -1)
+    else:
+        returen -1
